c7833d8d829d1e3f94569483c276dc385c646820
PerryTheShark <86941913+PerryTheShark@users.noreply.github.com>
2024-12-14 15:03:35 +0700

    Create day34.js

---
+++ day34.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(arr) {
+        // Sort the array of strings
+    arr.sort();
+
+    // Get the first and last strings after sorting
+    let first = arr[0];
+    let last = arr[arr.length - 1];
+    let minLength = Math.min(first.length, last.length);
+
+    let i = 0;
+    
+    // Find the common prefix between the first and 
+    // last strings
+    while (i < minLength && first[i] === last[i]) {
+        i++;
+    }
+
+    // Return the common prefix
+    return first.substring(0, i);
+};
